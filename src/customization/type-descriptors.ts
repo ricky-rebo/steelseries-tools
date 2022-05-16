3fac2310eb869c350a691e0e092f219107cf356c
Riccardo Rebottini <ricky@rebottini.it>
2022-05-16 14:54:55 +0200

    :recycle: fix type defs

---
+++ src/customization/type-descriptors.ts
@@ -0,0 +1,37 @@
+class TypeDef {
+  type: string
+  constructor (type: string) { this.type = type }
+}
+
+
+export class GaugeTypeDef extends TypeDef {}
+
+export class OrientationDef extends TypeDef {}
+
+export class KnobTypeDef extends TypeDef {}
+
+export class KnobStyleDef {
+  style: string
+  constructor (style: string) { this.style = style }
+}
+
+export class FrameDesignDef {
+  design: string
+  constructor (design: string) { this.design = design }
+}
+
+export class PointerTypeDef extends TypeDef {}
+
+export class ForegroundTypeDef extends TypeDef {}
+
+export class LabelNumberFormatDef {
+  format: string
+  constructor (format: string) { this.format = format }
+}
+
+export class TickLabelOrientationDef extends TypeDef {}
+
+export class TrendStateDef {
+  state: string
+  constructor (state: string) { this.state = state }
+}
