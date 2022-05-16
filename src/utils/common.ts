2b2906a711961679e5216fe814b247c93bed8969
Riccardo Rebottini <ricky@rebottini.it>
2022-05-16 17:12:44 +0200

    :sparkles: add createBuffer() and constants utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,12 @@
 export function range (value: number, limit: number) {
   return value < 0 ? 0 : value > limit ? limit : value
 }
+
+export function createBuffer (width: number, height = width, id?: string) {
+  const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  canvas.id = id ? id : ''
+
+  return canvas
+}
