8211477303a270d6867c1d10e1d5cb776a43da0f
Riccardo Rebottini <ricky@rebottini.it>
2022-05-19 19:59:15 +0200

    :label: add typings for drawRadialCustomInage()

---
+++ src/draw/drawRadialCustomImage.ts
@@ -0,0 +1,26 @@
+import { TWO_PI } from "../utils/constants"
+
+export function drawRadialCustomImage (ctx: CanvasRenderingContext2D, img: CanvasImageSource) {
+  const width = ctx.canvas.width
+  const height = ctx.canvas.height
+  
+  const drawWidth = width * 0.831775
+  const drawHeight = height * 0.831775
+
+  const startX = (width - drawWidth) / 2
+  const startY = (height - drawHeight) / 2
+
+  if (img !== null && img.height > 0 && img.width > 0) {
+    ctx.save()
+
+    // Set the clipping area
+    ctx.beginPath()
+    ctx.arc(width/2, height/2, drawWidth/2, 0, TWO_PI, true)
+    ctx.clip()
+
+    // Add the image
+    ctx.drawImage(img, startX, startY, drawWidth, drawHeight)
+
+    ctx.restore()
+  }
+}
