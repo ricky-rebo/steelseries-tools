b27a510381eb339e65c5e5f71e367e8858dada8a
Riccardo Rebottini <ricky@rebottini.it>
2022-05-29 18:15:11 +0200

    :label: add global utility types

---
+++ types/index..d.ts
@@ -0,0 +1,8 @@
+declare var __DEV__: boolean
+
+declare type CanvasCtx = CanvasRenderingContext2D
+
+declare type CanvasCache = Record<string, HTMLCanvasElement>
+
+declare type RgbColorString = `rgb(${number},${number},${number})`
+declare type RgbaColorString = `rgb(${number},${number},${number},${number})`
