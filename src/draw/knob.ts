1fc470743562b85cfa42e36d53517d7712f6d308
Riccardo Rebottini <ricky@rebottini.it>
2022-05-30 17:25:29 +0200

    :recycle: extracted knob draw code from drawForeground()

--- src/draw/knob.ts
+++ src/draw/knob.ts
@@ -1,7 +1,31 @@
-import { KnobStyleDef } from '../customization/type-descriptors';
+import { createKnobImage } from '../create/createKnobImage';
+import { KnobStyleDef, KnobTypeDef } from '../customization/type-descriptors';
 import { TWO_PI } from "../utils/constants";
 
-export function drawStandardKnob(ctx: CanvasRenderingContext2D, size: number, knobStyle: KnobStyleDef) {
+export function drawKnob (ctx: CanvasCtx, type: KnobTypeDef, style: KnobStyleDef, width = ctx.canvas.width, height = ctx.canvas.height) {
+  const knobSize = Math.ceil(height * 0.084112)
+  const knobX = width * 0.5 - knobSize / 2
+  const knobY = height * 0.5 - knobSize / 2
+  const shadowOffset = width * 0.008
+
+  const knobImage = createKnobImage(knobSize, type, style)
+
+  ctx.save()
+
+  // Set the pointer shadow params
+  ctx.shadowColor = "rgba(0, 0, 0, 0.8)"
+  ctx.shadowOffsetX = ctx.shadowOffsetY = shadowOffset
+  ctx.shadowBlur = shadowOffset * 2
+
+  ctx.drawImage(knobImage, knobX, knobY)
+
+  ctx.restore()
+}
+
+
+/* Internal draw functions */
+
+export function drawStandardKnobImage(ctx: CanvasRenderingContext2D, size: number, knobStyle: KnobStyleDef) {
   const maxPostCenterX = size / 2;
   const maxPostCenterY = size / 2;
   let grad;
@@ -58,7 +82,8 @@ export function drawStandardKnob(ctx: CanvasRenderingContext2D, size: number, kn
   ctx.fillStyle = grad;
   ctx.fill();
 }
-export function drawMetalKnob(ctx: CanvasRenderingContext2D, size: number, knobStyle: KnobStyleDef) {
+
+export function drawMetalKnobImage(ctx: CanvasRenderingContext2D, size: number, knobStyle: KnobStyleDef) {
   let grad;
 
   // METALKNOB_FRAME
