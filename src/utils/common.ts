bc07ea78e9138ee82a1d2ec8650fa517a3f774e8
Riccardo Rebottini <ricky@rebottini.it>
2022-05-31 23:49:41 +0200

    :sparkles: add calcNiceNumber() utility

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,6 +2,35 @@ export function range (value: number, limit: number) {
   return value < 0 ? 0 : value > limit ? limit : value
 }
 
+export function calcNiceNumber (_range: number, round: boolean) {
+  const exponent = Math.floor(Math.log10(_range)) // exponent of range
+  const fraction = _range / Math.pow(10, exponent) // fractional part of range
+  let niceFraction // nice, rounded fraction
+
+  if (round) {
+    if (fraction < 1.5) {
+      niceFraction = 1
+    } else if (fraction < 3) {
+      niceFraction = 2
+    } else if (fraction < 7) {
+      niceFraction = 5
+    } else {
+      niceFraction = 10
+    }
+  } else {
+    if (fraction <= 1) {
+      niceFraction = 1
+    } else if (fraction <= 2) {
+      niceFraction = 2
+    } else if (fraction <= 5) {
+      niceFraction = 5
+    } else {
+      niceFraction = 10
+    }
+  }
+  return niceFraction * Math.pow(10, exponent)
+}
+
 export function createBuffer (width: number, height = width, id?: string) {
   const canvas = document.createElement('canvas')
   canvas.width = width
