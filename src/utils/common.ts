789081f5bdafa61c9a6abaad6b201436eb78b3a5
Riccardo Rebottini <ricky@rebottini.it>
2022-06-06 23:27:45 +0200

    :sparkles: add drawRoundedRectangle() util

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -54,6 +54,25 @@ export function drawToBuffer (width: number, height: number, drawFunction: DrawF
   return buffer
 }
 
+
+/** //TODO docs */
+export function drawRoundedRectangle (ctx: CanvasCtx, x: number, y: number, w: number, h: number, radius: number) {
+  const r = x + w
+  const b = y + h
+
+  ctx.beginPath()
+  ctx.moveTo(x + radius, y)
+  ctx.lineTo(r - radius, y)
+  ctx.quadraticCurveTo(r, y, r, y + radius)
+  ctx.lineTo(r, y + h - radius)
+  ctx.quadraticCurveTo(r, b, r - radius, b)
+  ctx.lineTo(x + radius, b)
+  ctx.quadraticCurveTo(x, b, x, b - radius)
+  ctx.lineTo(x, y + radius)
+  ctx.quadraticCurveTo(x, y, x + radius, y)
+  ctx.closePath()
+}
+
 export function rotateContext (ctx: CanvasRenderingContext2D, angle: number, centerX = ctx.canvas.width/2, centerY = ctx.canvas.height/2) {
   // const centerX = ctx.canvas.width / 2
   // const centerY = ctx.canvas.height / 2
