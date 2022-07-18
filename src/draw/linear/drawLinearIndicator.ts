6cfe6a30abf269e582d07fc2753ad4b805e14adb
Riccardo Rebottini <ricky@rebottini.it>
2022-07-18 18:04:02 +0200

    :label: add tryping for drawLinearIndicator()

--- src/draw/linear/drawLinearIndicator.ts
+++ src/draw/linear/drawLinearIndicator.ts
@@ -1,17 +1,28 @@
-function drawLinearIndicator (ctx, indicator, value, minValue, maxValue, gaugeType, vertical, altPos) {
+import { GaugeTypeDef } from "../../customization/type-descriptors"
+
+export function drawLinearIndicator (
+  ctx: CanvasCtx,
+  indicator: HTMLCanvasElement,
+  value: number,
+  minValue: number,
+  maxValue: number,
+  gaugeType: GaugeTypeDef,
+  vertical: boolean,
+  altPos: boolean
+) {
   const width = ctx.canvas.width
   const height = ctx.canvas.height
 
   let valuePos, yOffset, yRange, posX, posY
   if (vertical) {
-    yOffset = gaugeType.type === 'type2' ? 0.7475 : 0.856796
+    yOffset = (gaugeType.type === 'type2') ? 0.7475 : 0.856796
     yRange = yOffset - 0.12864
     valuePos = height * yOffset - (height * yRange * (value - minValue)) / (maxValue - minValue)
-    posX = altPos ? width * 0.34 - indicator.width : width * 0.365
+    posX = (altPos) ? width * 0.34 - indicator.width : width * 0.365
     posY = valuePos - indicator.height / 2
   } else {
-    const factor = gaugeType.type === 'type2' ? 0.19857 : 0.142857
-    yOffset = gaugeType.type === 'type2' ? 0.82 : 0.871012
+    const factor = (gaugeType.type === 'type2') ? 0.19857 : 0.142857
+    yOffset = (gaugeType.type === 'type2') ? 0.82 : 0.871012
     yRange = yOffset - factor
     valuePos = (width * yRange * (value - minValue)) / (maxValue - minValue)
     posX = width * factor - indicator.height / 2 + valuePos
@@ -22,5 +33,3 @@ function drawLinearIndicator (ctx, indicator, value, minValue, maxValue, gaugeTy
   ctx.drawImage(indicator, posX, posY)
   ctx.restore()
 }
-
-export default drawLinearIndicator
