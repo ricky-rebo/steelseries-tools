5379210ca799e7eb328975c623ca2268f064c4e5
Riccardo Rebottini <ricky@rebottini.it>
2022-07-21 22:45:29 +0200

    :construction: added all exports

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,53 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop')
-  }
-  return a + b;
-}
-
 // Main radial structure
 export { drawFrame } from "./draw/drawFrame"
 export { drawBackground } from "./draw/drawBackground";
 export { drawForeground } from "./draw/drawForeground"
 
+
 // Main linear structure
 export { drawLinearFrame } from "./draw/linear/drawLinearFrame";
 export { drawLinearBackground } from "./draw/linear/drawLinearBackground";
+export { drawLinearForeground } from "./draw/linear/drawLinearForeground";
+
+export { drawKnob } from "./draw/drawKnob";
 
 export { drawTickmarks } from "./draw/drawTickmarks";
 export { drawTickmarkLables } from "./draw/drawTickmarkLables";
 
-export { BackgroundColor } from "./customization/colors";
+export { drawActiveBargraphLed } from "./draw/drawActiveBargraphLed";
+export { drawPointerImage } from "./draw/drawPointerImage";
+export { drawRadialCustomImage } from "./draw/drawRadialCustomImage";
+export { drawRoseImage } from "./draw/drawRoseImage";
+export { drawStrings } from "./draw/drawStrings";
+
+export { drawLinearIndicator } from "./draw/linear/drawLinearIndicator";
+export { drawLinearStrings } from "./draw/linear/drawLinearStrings";
+export { drawLinearTickmarks } from "./draw/linear/drawLinearTickmarks";
+
+
+export { createLcdBackgroundImage } from "./create/createLcdBackgroundImage";
+export { createLedImage } from "./create/createLedImage";
+export { createMeasuredValueImage } from "./create/createMeasuredValueImage";
+export { createThresholdImage } from "./create/createThresholdImage";
+export { createTrendIndicator } from "./create/createTrendIndicator";
 
 export {
-  FrameDesign,
-  ForegroundType,
+  Color as ColorDef,
+  BackgroundColor,
+  LcdColor,
+  LedColor
+} from "./customization/colors";
+
+export {
   GaugeType,
+  Orientation,
+  KnobType,
+  KnobStyle,
+  FrameDesign,
+  PointerType,
+  ForegroundType,
   LabelNumberFormat,
-  TickLabelOrientation
+  TickLabelOrientation,
+  TrendState
 } from "./customization/types"
 
