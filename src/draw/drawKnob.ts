a683db54ee7cdf72a83a4e955ff337baade8a00f
Riccardo Rebottini <ricky@rebottini.it>
2022-07-21 22:44:52 +0200

    :adhesive_bandage: fixed circular dependency

---
+++ src/draw/drawKnob.ts
@@ -0,0 +1,23 @@
+import { createKnobImage } from '../create/createKnobImage';
+import { KnobStyleDef, KnobTypeDef } from '../customization/type-descriptors';
+
+// TODO docs
+export function drawKnob (ctx: CanvasCtx, type: KnobTypeDef, style: KnobStyleDef, width = ctx.canvas.width, height = ctx.canvas.height) {
+  const knobSize = Math.ceil(height * 0.084112)
+  const knobX = width * 0.5 - knobSize / 2
+  const knobY = height * 0.5 - knobSize / 2
+  const shadowOffset = width * 0.008
+
+  const knobImage = createKnobImage(knobSize, type, style)
+
+  ctx.save()
+
+  // Set the pointer shadow params
+  ctx.shadowColor = "rgba(0, 0, 0, 0.8)"
+  ctx.shadowOffsetX = ctx.shadowOffsetY = shadowOffset
+  ctx.shadowBlur = shadowOffset * 2
+
+  ctx.drawImage(knobImage, knobX, knobY)
+
+  ctx.restore()
+}
